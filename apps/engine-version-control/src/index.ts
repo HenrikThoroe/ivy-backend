fde66bf41738b1b0bf6034312097ac6e2fcc2f97
Henrik Thoroe <henrik@thoroee.com>
2023-06-22 18:23:46 +0200

    feat: add cors

--- apps/engine-version-control/src/index.ts
+++ apps/engine-version-control/src/index.ts
@@ -6,11 +6,13 @@ import express from 'express'
 import { engineRouter } from './routes/engine.route'
 import fileUpload from 'express-fileupload'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const app = express()
 const maxFileSize = 100 * 1024 * 1024
 
 app.use(helmet())
+app.use(cors())
 app.use(
   fileUpload({
     limits: {
--- apps/test-driver/src/index.ts
+++ apps/test-driver/src/index.ts
@@ -7,9 +7,11 @@ import helmet from 'helmet'
 import { suiteRouter } from './router/suite.routes'
 import { driverMgmtRouter, driverServer } from './router/driver.routes'
 import { sessionRouter } from './router/session.router'
+import cors from 'cors'
 const app = express()
 
 app.use(helmet())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/suites', suiteRouter)
