05b6df1ef66796acd3863eb23fd5461f327363bf
Henrik Thoroe <henrik@thoroee.com>
2023-07-03 21:47:21 +0200

    fix(model): add timeback to create options params

--- packages/model/__tests__/game/actions/register.test.ts
+++ packages/model/__tests__/game/actions/register.test.ts
@@ -3,21 +3,21 @@ import { register } from '../../../src/game/actions/register'
 
 describe('Game Player Registration', () => {
   it('creates different ids', () => {
-    const game = create({ timeout: 1000 })
+    const game = create({ timeout: 1000, timeback: 0 })
     const ids = [register(game, 'black'), register(game, 'white')]
 
     expect(ids[0]).not.toBe(ids[1])
   })
 
   it('assigns both slots when no color is requested', () => {
-    const game = create({ timeout: 1000 })
+    const game = create({ timeout: 1000, timeback: 0 })
     const ids = [register(game), register(game)]
 
     expect(ids[0]).not.toBe(ids[1])
   })
 
   it('throws on more thann two attempts', () => {
-    const game = create({ timeout: 1000 })
+    const game = create({ timeout: 1000, timeback: 0 })
 
     register(game)
     register(game)
@@ -28,7 +28,7 @@ describe('Game Player Registration', () => {
   })
 
   it('throws when assigning a color twice', () => {
-    const game = create({ timeout: 1000 })
+    const game = create({ timeout: 1000, timeback: 0 })
     register(game, 'white')
     expect(() => register(game, 'white')).toThrow()
   })
