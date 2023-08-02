08f20dd337645a9b89db8e94b1a04da553cbad8f
Henrik Thoroe <henrik@thoroee.com>
2023-08-02 10:14:33 +0200

    feat(com): add queue and payload to fetch replays

--- packages/com/src/queues.ts
+++ packages/com/src/queues.ts
@@ -1,9 +1,11 @@
-type Service = 'replay' | 'replay-log'
+type Service = 'replay' | 'replay-log' | 'stats'
 
 type Worker<T extends Service> = T extends 'replay'
-  ? 'save'
+  ? 'save' | 'fetch'
   : T extends 'replay-log'
   ? 'save'
+  : T extends 'stats'
+  ? 'replay'
   : never
 
 export function queueName<T extends Service>(service: T, worker: Worker<T>) {
