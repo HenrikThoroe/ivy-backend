0975aaf58d014e09285d9597f65220afd699fb95
Henrik Thoroe <henrik@thoroee.com>
2023-08-02 09:58:45 +0200

    feat(model): add method to hash engine test configs

--- packages/model/src/test/TestSuite.ts
+++ packages/model/src/test/TestSuite.ts
@@ -1,4 +1,5 @@
 import { EngineInstance } from '../configs/EngineConfig'
+import crypto from 'crypto'
 
 export interface EngineTestConfig extends EngineInstance {
   timeControl: {
@@ -16,4 +17,19 @@ export interface TestSuite {
   id: string
   iterations: number
   engines: [EngineTestConfig, EngineTestConfig]
+}
+
+export function hashEngineTestConfig(config: EngineTestConfig): string {
+  const hash = crypto.createHash('sha256')
+
+  hash.update(config.name)
+  hash.update(config.version.major.toString())
+  hash.update(config.version.minor.toString())
+  hash.update(config.version.patch.toString())
+  hash.update(config.timeControl.type)
+  hash.update(config.timeControl.value.toString())
+  hash.update(config.options.threads.toString())
+  hash.update(config.options.hash.toString())
+
+  return hash.digest('hex')
 }
