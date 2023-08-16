073e1d8067228ababe66675b7ebfc6d925a56477
Henrik Thoroe <henrik@thoroee.com>
2023-08-16 22:53:34 +0200

    feat(schema): export shared schemas

--- packages/api-schema/src/index.ts
+++ packages/api-schema/src/index.ts
@@ -1,3 +1,4 @@
 export * as api from './schema'
 export * from './types/endpoint'
 export * from './types/route'
+export * as shared from './shared'
---
+++ packages/api-schema/src/shared/index.ts
@@ -0,0 +1,4 @@
+export * as engine from './engine'
+export * as game from './game'
+export * as generic from './generic'
+export * as replay from './replay'
