d0cfbd31e57de92cffb42c7faf432cf25919afd4
Henrik Thoroe <henrik@thoroee.com>
2023-08-25 15:47:42 +0200

    feat(schema): add types for test data

--- packages/api-schema/src/shared/replay.ts
+++ packages/api-schema/src/shared/replay.ts
@@ -1,19 +1,30 @@
-import { UCILog, ReplayLog, MoveInfo, Replay, TestDriver, ReplayStats } from '@ivy-chess/model'
+import {
+  MoveInfo,
+  Replay,
+  ReplayLog,
+  ReplayStats,
+  TestDriver,
+  TestDriverHardware,
+  UCILog,
+} from '@ivy-chess/model'
 import { z } from 'zod'
-import { moveSchema } from './game'
 import { engineTestConfigSchema } from './engine'
+import { moveSchema } from './game'
 import { nameSchema } from './generic'
+
+/**
+ * Schema for log entries.
+ */
+export const logEntrySchema = z.object({
+  type: z.enum(['recv', 'send']),
+  value: z.string(),
+})
 
 /**
  * Schema for {@link UCILog}.
  */
 export const uciLogSchema = z.object({
-  messages: z.array(
-    z.object({
-      type: z.enum(['recv', 'send']),
-      value: z.string(),
-    })
-  ),
+  messages: z.array(logEntrySchema),
 }) satisfies z.ZodType<UCILog>
 
 /**
@@ -26,38 +37,71 @@ export const replayLogSchema = z.object({
 }) satisfies z.ZodType<ReplayLog>
 
 /**
+ * Schema for UCI move details.
+ */
+export const moveDetailsSchema = z.object({
+  depth: z.number().int().positive(),
+  selDepth: z.number().int().positive(),
+  time: z.number().int().positive(),
+  nodes: z.number().int().positive(),
+  pv: z.array(z.string()),
+  multiPv: z.number().int().positive(),
+  score: z.object({
+    type: z.enum(['cp', 'mate']),
+    value: z.number().int(),
+    lowerbound: z.boolean(),
+    upperbound: z.boolean(),
+  }),
+  currentMove: z.string(),
+  currentMoveNumber: z.number().int().positive(),
+  hashFull: z.number().int().positive(),
+  nps: z.number().int().positive(),
+  tbHits: z.number().int().positive(),
+  sbhits: z.number().int().positive(),
+  cpuLoad: z.number().int().positive(),
+  string: z.string(),
+  refutation: z.array(z.string()),
+  currline: z.array(z.string()),
+})
+
+/**
  * Schema for {@link MoveInfo}.
  */
 export const moveInfoSchema = z.object({
   move: moveSchema,
   fen: z.string(),
-  details: z
-    .object({
-      depth: z.number().int().positive(),
-      selDepth: z.number().int().positive(),
-      time: z.number().int().positive(),
-      nodes: z.number().int().positive(),
-      pv: z.array(z.string()),
-      multiPv: z.number().int().positive(),
-      score: z.object({
-        type: z.enum(['cp', 'mate']),
-        value: z.number().int(),
-        lowerbound: z.boolean(),
-        upperbound: z.boolean(),
-      }),
-      currentMove: z.string(),
-      currentMoveNumber: z.number().int().positive(),
-      hashFull: z.number().int().positive(),
-      nps: z.number().int().positive(),
-      tbHits: z.number().int().positive(),
-      sbhits: z.number().int().positive(),
-      cpuLoad: z.number().int().positive(),
-      string: z.string(),
-      refutation: z.array(z.string()),
-      currline: z.array(z.string()),
-    })
-    .partial(),
+  details: moveDetailsSchema.partial(),
 }) satisfies z.ZodType<MoveInfo>
+
+/**
+ * Schema for {@link TestDriverHardware}.
+ */
+export const testDriverHardwareSchema = z.object({
+  cpu: z
+    .array(
+      z.object({
+        model: z.string().optional(),
+        vendor: z.string().optional(),
+        cores: z.number().int().positive().optional(),
+        threads: z.number().int().positive().optional(),
+        capabilities: z.array(z.string()).optional(),
+      }),
+    )
+    .optional(),
+  memory: z.number().int().positive().optional(),
+  gpu: z
+    .array(
+      z.object({
+        model: z.string().optional(),
+        vendor: z.string().optional(),
+        memory: z.number().int().positive().optional(),
+      }),
+    )
+    .optional(),
+  model: z.string().optional(),
+  os: z.string().optional(),
+  arch: z.string().optional(),
+}) satisfies z.ZodType<TestDriverHardware>
 
 /**
  * Schema for {@link TestDriver}.
@@ -65,32 +109,7 @@ export const moveInfoSchema = z.object({
 export const testDriverSchema = z.object({
   id: z.string().nonempty(),
   name: nameSchema,
-  hardware: z.object({
-    cpu: z
-      .array(
-        z.object({
-          model: z.string().optional(),
-          vendor: z.string().optional(),
-          cores: z.number().int().positive().optional(),
-          threads: z.number().int().positive().optional(),
-          capabilities: z.array(z.string()).optional(),
-        })
-      )
-      .optional(),
-    memory: z.number().int().positive().optional(),
-    gpu: z
-      .array(
-        z.object({
-          model: z.string().optional(),
-          vendor: z.string().optional(),
-          memory: z.number().int().positive().optional(),
-        })
-      )
-      .optional(),
-    model: z.string().optional(),
-    os: z.string().optional(),
-    arch: z.string().optional(),
-  }),
+  hardware: testDriverHardwareSchema,
 }) satisfies z.ZodType<TestDriver>
 
 /**
