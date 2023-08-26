d95baf72109ace0469967787c2e65edb7f013620
Henrik Thoroe <henrik@thoroee.com>
2023-08-26 21:06:46 +0200

    feat(test-driver): rewrite external API to use updated packages

---
+++ apps/test-driver/src/api/driver.router.ts
@@ -0,0 +1,25 @@
+import { api } from '@ivy-chess/api-schema'
+import { router } from 'rest'
+import { ClientManager } from '../service/ClientManager'
+
+/**
+ * Router for test driver related requests.
+ */
+export const driverRouter = router(api.testing.http.driverRoute, {
+  all: async ({}, success, _) => {
+    const clients = ClientManager.shared.all
+    const drivers = clients.map((c) => c.driver)
+
+    return success(drivers)
+  },
+
+  get: async ({ params: { id } }, success, failure) => {
+    const driver = ClientManager.shared.fetch(id)?.driver
+
+    if (driver) {
+      return success(driver)
+    }
+
+    return failure({ message: `Test driver with id '${id}' does not exist.` }, 404)
+  },
+})
--- apps/test-driver/src/controller/session.types.ts
+++
@@ -1,6 +0,0 @@
-import { Array, Union, String, Number, Record } from 'runtypes'
-
-export const CreateBody = Record({
-  driver: Number.withConstraint((value) => value > 0),
-  suite: String,
-})
