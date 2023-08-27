74c76489d38e358090fe3eb36a099b37c753f039
Henrik Thoroe <henrik@thoroee.com>
2023-08-27 15:13:57 +0200

    feat(gm): rewrite external API with updated packages

--- apps/game-manager/src/controller/management.types.ts
+++
@@ -1,6 +0,0 @@
-import { Number, Record } from 'runtypes'
-
-export const CreateBody = Record({
-  timeout: Number.withConstraint((val) => val > 100 && val < 24 * 60 * 60 * 1000),
-  timeback: Number.withConstraint((val) => val >= 0),
-})
--- apps/game-manager/src/router/manage.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { exceptionWrapper } from 'express-util'
-import { handleGameCreation } from '../controller/management.controller'
-
-export const managementRouter = Router()
-
-managementRouter.post('/', exceptionWrapper(handleGameCreation))
