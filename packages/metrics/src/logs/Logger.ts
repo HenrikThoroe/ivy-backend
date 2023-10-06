0a574adf4ff9d10acedfcbe4d7c970141b603c39
Henrik Thoroe <henrik@thoroee.com>
2023-10-06 11:46:10 +0200

    fix(metrics): return `0 B` when 0 is given for memory size

--- packages/metrics/src/logs/Logger.ts
+++ packages/metrics/src/logs/Logger.ts
@@ -122,6 +122,10 @@ export abstract class Logger<D extends LogData> {
   }
 
   protected formatMemSize(bytes: number, decimals: number = 2) {
+    if (bytes === 0) {
+      return '0 B'
+    }
+
     const k = 1000
     const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
     const i = Math.floor(Math.log(bytes) / Math.log(k))
