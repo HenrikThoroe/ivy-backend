dc1ef510d0c3c85c71d368f4c0765f1acbfaf244
Henrik Thoroe <henrik@thoroee.com>
2023-10-06 15:21:07 +0200

    feat(global): log HTTP + WS routes

--- apps/stats/src/index.ts
+++ apps/stats/src/index.ts
@@ -2,18 +2,20 @@ import cors from 'cors'
 import { loadenv } from 'env-util'
 import express from 'express'
 import helmet from 'helmet'
+import { HTTPLogger } from 'metrics'
 import { setupMessageQueue } from './api/messages'
 import { verificationRouter } from './api/router'
 
 loadenv()
 
 const app = express()
+const logger = new HTTPLogger('Verification')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
-verificationRouter.mount(app)
+verificationRouter.mount(app, logger)
 app.listen(process.env.STATS_PORT)
 
 setupMessageQueue()
