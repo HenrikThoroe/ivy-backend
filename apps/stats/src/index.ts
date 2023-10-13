0c838892b2afe9b1f64b4f103fd9f9c626a4f488
Henrik Thoroe <henrik@thoroee.com>
2023-10-13 21:25:40 +0200

    feat(global): use authentication handler

--- apps/stats/src/index.ts
+++ apps/stats/src/index.ts
@@ -1,3 +1,4 @@
+import { AuthFactory } from 'auth'
 import cors from 'cors'
 import { loadenv } from 'env-util'
 import express from 'express'
@@ -10,12 +11,18 @@ loadenv()
 
 const app = express()
 const logger = new HTTPLogger('Verification')
+const { handler } = AuthFactory.supabase({
+  url: process.env.SUPABASE_URL!,
+  key: process.env.SUPABASE_KEY!,
+  secret: process.env.JWT_SECRET!,
+  issuer: process.env.JWT_ISSUER!,
+})
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
-verificationRouter.mount(app, logger)
+verificationRouter.mount(app, logger, handler)
 app.listen(process.env.STATS_PORT)
 
 setupMessageQueue()
--- apps/test-driver/src/index.ts
+++ apps/test-driver/src/index.ts
@@ -1,3 +1,4 @@
+import { AuthFactory } from 'auth'
 import cors from 'cors'
 import { loadenv } from 'env-util'
 import express from 'express'
@@ -9,6 +10,14 @@ import { sessionRouter } from './api/session.router'
 import { suiteRouter } from './api/suite.router'
 
 const app = express()
+
+const { handler } = AuthFactory.supabase({
+  url: process.env.SUPABASE_URL!,
+  key: process.env.SUPABASE_KEY!,
+  secret: process.env.JWT_SECRET!,
+  issuer: process.env.JWT_ISSUER!,
+})
+
 const logger = {
   driver: new HTTPLogger('Driver'),
   session: new HTTPLogger('Sessions'),
@@ -22,9 +31,9 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-driverRouter.mount(app, logger.driver)
-suiteRouter.mount(app, logger.suite)
-sessionRouter.mount(app, logger.session)
+driverRouter.mount(app, logger.driver, handler)
+suiteRouter.mount(app, logger.suite, handler)
+sessionRouter.mount(app, logger.session, handler)
 driverSocket.use(logger.clients)
 driverSocket.listen(process.env.TEST_WSS_PORT!)
 app.listen(process.env.TEST_PORT!)
