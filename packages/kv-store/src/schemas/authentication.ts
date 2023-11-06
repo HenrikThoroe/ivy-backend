0990152d62a293f1915304df0e9a362d5eb58a36
Henrik Thoroe <henrik@thoroee.com>
2023-11-06 20:02:36 +0100

    feat(kv-store): store manager IDs

--- packages/kv-store/src/schemas/authentication.ts
+++ packages/kv-store/src/schemas/authentication.ts
@@ -5,5 +5,6 @@ import { DynamicStore, StaticStore } from '../stores'
  * Store for all authentication related data.
  */
 export const authenticationStore = StaticStore.redis('authentication', {
+  managers: 'set',
   users: new DynamicStore('users', shared.user.userSchema),
 })
