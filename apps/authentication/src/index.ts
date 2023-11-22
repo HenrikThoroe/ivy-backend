441bc4650e4a2b9602fa7e98d194d01542b3953e
Henrik Thoroe <henrik@thoroee.com>
2023-11-22 22:36:04 +0100

    feat(global): use local auth when NODE_ENV is set to `test`

--- apps/authentication/src/index.ts
+++ apps/authentication/src/index.ts
@@ -10,7 +10,8 @@ loadenv()
 
 const app = express()
 const logger = new HTTPLogger('auth')
-const { handler } = AuthFactory.supabase({
+const factory = process.env.NODE_ENV === 'test' ? AuthFactory.local : AuthFactory.supabase
+const { handler } = factory({
   url: process.env.SUPABASE_URL!,
   key: process.env.SUPABASE_KEY!,
   secret: process.env.JWT_SECRET!,
